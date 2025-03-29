e5a83ad1ded8cac641d63daaf6ff87782d5e4151
Christine <chanc245@newschool.edu>
2025-03-29 03:28:33 -0400

    update// welcome page //more refine detiles

--- public/welcome.js
+++ public/welcome.js
@@ -1,4 +1,14 @@
 function zoomLeaf() {
   const svg = document.getElementById("intro-leaf");
+  // start the zoom animation
   svg.style.transform = "translate(-50%, -50%) scale(20)";
+
+  // end of the transition, then go to index.html
+  svg.addEventListener(
+    "transitionend",
+    function () {
+      window.location.href = "index.html";
+    },
+    { once: true }
+  );
 }
